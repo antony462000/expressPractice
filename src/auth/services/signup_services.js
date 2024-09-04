3a3f59e9c2d78fc1e4cb768d2cfece6b44590b81
antony462000 <antonycyriact@gmail.com>
2024-09-04 12:05:16 +0530

    Signup Commits

--- src/auth/services/signup_services.js
+++ src/auth/services/signup_services.js
@@ -1,7 +1,40 @@
-module.exports = (profiles) =>{
+const fs = require('fs')
+
+module.exports = (profiles) => {
+    // try {
+    //     
+    //     const allData = fs.readFileSync('db.json','utf8'
+    //    var variable = allData.toString('base64')
+    //    let len = variable.length
+    // //    let dd = JSON.parse(allData)
+    //    console.log(typeof(allData))
+    //     if (len==0){
+    //         console.log(" NULL")
+    //         // console.log(allData.toString('base64'))
+    //         const jsonStringN = JSON.stringify(profiles)
+    //         fs.writeFileSync('db.json',jsonStringN,'utf8')
+    //     }else{       
+    //         console.log("NOT NULL")
+    //         const allDbData = require('../../../db.json')
+    //         profiles.map((_)=> allDbData.push(_))
+    //         const jsonString = JSON.stringify(allDbData)
+    //         fs.writeFileSync('db.json',jsonString,'utf8')
+    //     }
+    // } catch (error) {
+    //     console.log("sdfasfasdf")
+    //     console.log(error)
+    // }
+
+
     try {
-        return(profiles)
+
+        const isDataExist = fs.readFileSync('db.json', 'utf8').toString()
+        let jsonData = isDataExist ? JSON.parse(isDataExist) : []
+
+        profiles.map((_) => jsonData.push(_))
+        fs.writeFileSync('db.json', JSON.stringify(jsonData), 'utf8')
+
     } catch (error) {
-        console.log("***ERROR***")
+        console.log(error)
     }
 }
