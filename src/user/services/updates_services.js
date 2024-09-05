e4b2cdbefaa70195989727e7298639aed314d6a4
antony462000 <antonycyriact@gmail.com>
2024-09-05 12:05:50 +0530

    JUST

--- src/user/services/updates_services.js
+++ src/user/services/updates_services.js
@@ -1,22 +1,21 @@
 const { error } = require('console')
 let db = require('../../../db.json')
 const fs = require('fs')
-module.exports =(data) =>{
-        let cid = data.id
-        db.map((_) => {
-            if (_.id==cid) {
-                _.name = (data.name)? data.name :_.name
-                _.password = (data.password)? data.password :_.password
-                _.mob = (data.mob)? data.mob :_.mob
-                _.email = (data.email)? data.email :_.email
-                console.log(_.name)
-            }
-            else{
-                throw new Error("INVALID ID")
-            }
-            
-        })
-        console.log(db)
-        fs.writeFileSync('db.json',JSON.stringify(db),'utf8')
-        return db  
+module.exports = (data) => {
+    db.map((_) => {
+        if (_.id == data.id) {
+            _.name = (data.name) ? data.name : _.name
+            _.password = (data.password) ? data.password : _.password
+            _.mob = (data.mob) ? data.mob : _.mob
+            _.email = (data.email) ? data.email : _.email
+            console.log(_.name)
+        }
+        else {
+            throw new Error("INVALID ID")
+        }
+
+    })
+    console.log(db)
+    fs.writeFileSync('db.json', JSON.stringify(db), 'utf8')
+    return db
 }
